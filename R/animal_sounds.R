2975dba9c761842621ffd0f0e1f5c33272756c12
WNaxin <naxinwang@outlook.com>
2024-06-26 13:21:23 +0100

    Added documentation

--- R/animal_sounds.R
+++ R/animal_sounds.R
@@ -1,3 +1,16 @@
+#' Animal Sounds Sentence Maker
+#'
+#' Outputs a sentence of what **sounds** a given *animal* makes using [this
+#' function][paste0()].
+#'
+#' @param animal name for the animal
+#' @param sound sound that the animal makes
+#'
+#' @return a sentence in the format of what the animal says
+#' @export
+#'
+#' @examples
+#' animal_sounds("dog", "woof")
 animal_sounds <- function(animal, sound) {
   stopifnot(is.character(animal) & length(animal) == 1)
   stopifnot(is.character(sound) & length(sound) == 1)

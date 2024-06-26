2007afb6b3fc4fa08b99fdb9be44deb7e7267dd6
WNaxin <naxinwang@outlook.com>
2024-06-26 15:05:44 +0100

    expect_error test

--- tests/testthat/test-animal_sounds.R
+++ tests/testthat/test-animal_sounds.R
@@ -8,4 +8,6 @@ test_that("animal_sounds produces expected strings", {
 test_that("handles invalid inputs", {
   expect_error(animal_sounds("dog", c("woof", "bow wow wow")),
                class = "error_not_single_string")
+  expect_error(animal_sounds(c("dog","cat"), "haha"),
+               class = "error_not_single_string")
 })

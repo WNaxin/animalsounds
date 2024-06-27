25dfe18fdfedd70993690a829d2ca761842bd28d
WNaxin <naxinwang@outlook.com>
2024-06-27 11:00:48 +0100

    add document data

---
+++ R/data.R
@@ -0,0 +1,10 @@
+#'Farm Animals and their sounds
+#'
+#'A dataset of farm animals and the sounds they make.
+#'
+#'
+#' @format A dataframe with 3 rows and 2 variables:
+#'  \describe{
+#'    \item{animal}{The name of the animal}
+#'    \item{sound}{The sound the animal makes}}
+"farm_animals"
---
+++ data-raw/farm_animals.R
@@ -0,0 +1,12 @@
+## code to prepare `farm_animals` dataset goes here
+
+animals <- c("dog","cat","elepphant")
+sounds <- c("woof","miaow","eeeeee")
+
+farm_animals <- data.frame(animal = animals, sound = sounds)
+
+
+usethis::use_data(farm_animals, overwrite = TRUE)
+
+
+
